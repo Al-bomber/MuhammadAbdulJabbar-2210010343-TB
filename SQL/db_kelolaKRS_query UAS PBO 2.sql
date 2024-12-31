aeb94e97ab6c445524a01870b44f2f2fca286eba
Al-bomber <adljabbar21@gmail.com>
2024-12-31 23:58:58 +0800

    Create Aplikasi Pengelola KRS
    Menambahkan Aplikasi Pengelola KRS ke Repo

---
+++ SQL/db_kelolaKRS_query UAS PBO 2.sql
@@ -0,0 +1,24 @@
+/*
+CREATE TABLE krs (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nama VARCHAR(20),
+	npm INT,
+	semester VARCHAR(20),
+	kelas VARCHAR(20),
+	matkul_1 VARCHAR(25),
+	matkul_2 VARCHAR(25),
+	matkul_3 VARCHAR(25),
+	matkul_4 VARCHAR(25),
+	matkul_5 VARCHAR(25),
+	matkul_6 VARCHAR(25)
+);
+
+
+CREATE TABLE bayar (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nama VARCHAR(20),
+	npm INT,
+	semester VARCHAR(20),
+	tipe VARCHAR(20),
+	total_bayar VARCHAR(20)
+);*/
